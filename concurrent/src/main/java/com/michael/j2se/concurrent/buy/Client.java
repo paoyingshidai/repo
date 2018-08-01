f71e880694b9229e3fac79ec492a28c3ef4edd96
Michael <13432849094@163.com>
2018-08-01 20:28:39 +0800

    commit

--- concurrent/src/main/java/com/michael/j2se/concurrent/buy/Client.java
+++ concurrent/src/main/java/com/michael/j2se/concurrent/buy/Client.java
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-package com.michael.j2se.concurrent.buy;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import com.michael.j2se.test.support.AbstractCountDownLatch;
-
-public class Client {
-
-	private List<User> users;
-	UserCurrent currentUtil;
-
-	public Client() {
-		this.users = new ArrayList<>();
-		Shop shop = new Shop(new AtomicInteger(9));						
-		this.currentUtil = new UserCurrent(shop, 9);
-	}
-	
-	public static void main(String[] args) {
-		
-		Client client = new Client();
-		client.currentUtil.start();
-		client.currentUtil.isLog(false);
-		client.users.forEach((u) -> {System.out.println(u == null ? null : u.goods );});
-	}
-	
-	
-	class UserCurrent extends AbstractCountDownLatch {
-		private Shop shop;
-		public UserCurrent(Shop shop, int threadNumber) {
-			super(threadNumber);
-			this.shop = shop;
-		}
-		@Override
-		protected void doWork() {
-			 User u = new User(shop);
-			 synchronized (u) {
-				 u.buy();
-			}
-			 users.add(u);
-		}
-	}
-}
-=======
 package com.michael.j2se.concurrent.buy;
 
 import java.util.ArrayList;
@@ -85,4 +39,3 @@ public class Client {
 		}
 	}
 }
->>>>>>> branch 'master' of https://github.com/paoyingshidai/javase.git
--- concurrent/src/main/java/com/michael/j2se/concurrent/buy/Shop.java
+++ concurrent/src/main/java/com/michael/j2se/concurrent/buy/Shop.java
@@ -1,53 +1,9 @@
-<<<<<<< HEAD
-package com.michael.j2se.concurrent.buy;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class Shop {
-
-//	/**
-//	 * 用于表示商品
-//	 */
-//	public Integer goodsNumber;
-//	
-//	/** 商品的数量 */
-//	public Shop(Integer goodsNumber) {
-//		this.goodsNumber = goodsNumber;
-//	}
-//	
-//	public void addGoods() {
-//		this.goodsNumber ++;
-//	}
-//	
-//	public Integer buyGoods() {
-//		return this.goodsNumber == 0 ? null : this.goodsNumber--;
-//	}
-	
-	/**
-	 * 用于表示商品
-	 */
-	public volatile AtomicInteger goodsNumber;
-	
-	/** 商品的数量 */
-	public Shop(AtomicInteger goodsNumber) {
-		this.goodsNumber = goodsNumber;
-	}
-	
-	public void addGoods() {
-		this.goodsNumber.incrementAndGet();
-	}
-	
-	public  Integer buyGoods() {
-		return this.goodsNumber.intValue() == 0 ? null : this.goodsNumber.decrementAndGet();
-	}
-	
-}
-=======
 package com.michael.j2se.concurrent.buy;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Shop {
 
@@ -80,4 +36,3 @@ public class Shop {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/paoyingshidai/javase.git
