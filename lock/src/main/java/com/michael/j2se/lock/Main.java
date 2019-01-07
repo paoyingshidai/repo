03b6e1a4ad65dea89ad5e22c5e38aead766a8f62
Michael <13432849094@163.com>
2019-01-08 00:08:32 +0800

    创建 lock 模块

---
+++ lock/src/main/java/com/michael/j2se/lock/Main.java
@@ -0,0 +1,8 @@
+package com.michael.j2se.lock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Main {
+
+
+}
---
+++ lock/src/main/java/com/michael/j2se/lock/ReentrantLockMain.java
@@ -0,0 +1,7 @@
+package com.michael.j2se.lock;
+
+public class ReentrantLockMain {
+
+
+
+}
