95dd040f42a3969acb487c39dd6de69e0233b4e2
Michael <13432849094@163.com>
2019-08-31 14:45:26 +0800

    线程池的多任务返回值的接受问题 CompletionService

--- thread/src/main/java/com/michael/j2se/thread/service/MultiReturn.java
+++ thread/src/main/java/com/michael/j2se/thread/service/MultiReturn.java
@@ -1,6 +1,9 @@
 package com.michael.j2se.thread.service;
 
+import lombok.*;
+
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 线程池多个返回值如何接收
@@ -10,33 +13,54 @@ public class MultiReturn {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-
-        ExecutorService executorService = new ThreadPoolExecutor(2, 2,
+//        corePoolSize -> 任务队列 -> maximumPoolSize -> 拒绝策略
+        ExecutorService executorService = new ThreadPoolExecutor(20, 20,
                 0, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(512),
-                new ThreadPoolExecutor.DiscardPolicy());// 指定拒绝策略
+                new ArrayBlockingQueue<>(32),
+                new ThreadPoolExecutor.DiscardPolicy()); // 指定拒绝策略
 
-        CompletionService<Result> ecs = new ExecutorCompletionService<>(executorService);// 构造器
+        CompletionService<Result> ecs = new ExecutorCompletionService<>(executorService); // 构造器
 
-        for (int i = 0; i < 10; i++) {
+        int taskSize = 20;
+
+        for (int i = 0; i < taskSize; i++) {
             // 注意，这里使用的是 CompletionService.submit()
             ecs.submit(() -> {
+                TimeUnit.SECONDS.sleep(1);
                 return new Result();
             });
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < taskSize; i++) {
             Result result = ecs.take().get();
-            System.out.println(result);
+            System.out.println(i);
+            System.out.println(result.toString());
         }
 
     }
 
 
+    @Getter
+    @Setter
     static class Result {
 
+        static AtomicInteger order = new AtomicInteger(0);
+
+        private int index;
+
+        Result() {
+            index = order.getAndAdd(1);
+            System.out.println(index);
+        }
+
+        public static int getOrder() {
+            return order.get();
+        }
+
+        @Override
+        public String toString() {
+            return "Result(order = " + this.index + " )";
+        }
     }
-
-
 
 }
