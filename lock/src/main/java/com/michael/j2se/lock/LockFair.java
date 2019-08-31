ca9aca41ca9a803f15c98efa694a96f40f895020
Michael <13432849094@163.com>
2019-08-31 16:26:54 +0800

    公平锁实现交替打印数字(不使用 ReentrantLock)

--- lock/src/main/java/com/michael/j2se/lock/LockFair.java
+++ lock/src/main/java/com/michael/j2se/lock/LockFair.java
@@ -12,30 +12,71 @@ public class LockFair implements Runnable {
     // 当前的序号
     public static int currentIndex = 0;
 
+    public static int currentIndex2 = 0;
+
     @Override
     public void run() {
         while (true){
             lock.lock();
             try {
                 if (currentIndex < 100) {
-                    System.out.println("当前值：" + currentIndex++ + ", " + Thread.currentThread().getName()+" 获得锁");
+                    System.out.println("当前值：" + currentIndex++ + ", " + Thread.currentThread().getName() + " 获得锁");
                 } else {
                     break;
                 }
-            }finally {
+            } finally {
                 lock.unlock();
             }
         }
     }
-    public static void main(String []args){
 
-        LockFair lockFairTest = new LockFair();
-        Thread t1 = new Thread(lockFairTest);
-        Thread t2 = new Thread(lockFairTest);
+
+    /**
+     * 交替打印 1 —— 100， 不使用 ReentranLock 公平锁;
+     */
+    public void print() {
+
+        Runnable runnable  = new Runnable() {
+            @Override
+            public void run() {
+
+                while(currentIndex2 <= 100) {
+
+                    synchronized (this) {
+
+                        notify();
+
+                        System.out.println("当前值：" + currentIndex2++ + ", " + Thread.currentThread().getName() + " 获得锁");
+
+                        try {
+//                            TimeUnit.SECONDS.sleep(1);
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
         t1.start();
         t2.start();
 
-//        Thread t3 = new Thread(lockFairTest);
-//        t3.start();
+
+    }
+
+
+    public static void main(String []args) {
+
+        LockFair lockFairTest = new LockFair();
+
+        lockFairTest.print();
+
+//        Thread t1 = new Thread(lockFairTest);
+//        Thread t2 = new Thread(lockFairTest);
+//        t1.start();
+//        t2.start();
     }
 }
